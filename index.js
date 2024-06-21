21302c17a277be480544eec41b7fc439e85a0dd9
AAlverth <154723947+AAlverth@users.noreply.github.com>
2024-06-20 21:44:04 -0500

    in class changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Test" );
 
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('.nav-item.dropdown').hover(
       function() {
         $(this).find('.dropdown-menu').stop(true, true).slideDown(300);
@@ -9,5 +9,14 @@ $(document).ready(function() {
         $(this).find('.dropdown-menu').stop(true, true).slideUp(300);
       }
     );
+  });*/
+  $(document).ready(function () {
+    $(".dropdown").hover(
+  function () {
+     $('ul.dropdown-menu').slideDown('medium');
+  }, 
+  function () {
+     $('ul.dropdown-menu').slideUp('medium');
+  }
+);
   });
-  
